78144495006f3afd23721c6f2764725683a09051
roel <ash49901@gmail.com>
2026-01-26 23:19:54 +0900

    [FEAT/#5] 공지사항 관련 페이지 옆 nav 공지사항 focus 되도록 구현

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -6,8 +6,8 @@ export const navItems = [
   { key: 'realTimeControl', title: '실시간 관제', href: paths.dashboard.realTimeControl, icon: 'air-traffic-control' },
   { key: 'AIBestLocation', title: 'AI 최적위치', href: paths.dashboard.AIBestLocation, icon: 'brain' },
   { key: 'budgetSimulation', title: '예산 시뮬레이션', href: paths.dashboard.budgetSimulation, icon: 'currency-dollar' },
-  { key: 'notice', title: '공지사항', href: paths.dashboard.notice, icon: 'megaphone' },
+  { key: 'notice', title: '공지사항', href: paths.dashboard.notice, icon: 'megaphone', matcher: { type: 'startsWith', href: paths.dashboard.notice } },
 	// todo: 혹시나해서 남겨뒀습니다. 필요없으면 지우시면 될 듯 합니다.
-	{ key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
+	{ key: 'account', title: 'Account', href: paths.dashboard.account, icon: "user" },
 	{ key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
 ] satisfies NavItemConfig[];
