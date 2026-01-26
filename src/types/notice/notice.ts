95c87e1345e0dfec89a937298d276af9958434d9
roel <ash49901@gmail.com>
2026-01-26 23:20:43 +0900

    [FEAT/#5] 공지사항 관련 type 선언

---
+++ src/types/notice/notice.ts
@@ -0,0 +1,43 @@
+/**
+ * 공지 유형 (고정)
+ */
+export type NoticeType = "공지" | "점검" | "업데이트" | "이벤트";
+
+/**
+ * 공통 공지사항 속성
+ */
+export interface BaseNotice {
+	notice_cd: string;
+	notice_title: string;
+	notice_type: NoticeType;
+	create_dt: string;
+}
+
+/**
+ * 목록에 표시될 공지사항 타입 (고정 여부 필수)
+ */
+export interface Notice extends BaseNotice {
+	notice_fix_yn: boolean;
+}
+
+/**
+ * 공지사항 파일 타입
+ */
+export interface NoticeFile {
+	notice_file_cd: number;
+	notice_file_nm: string;
+	notice_file_type: string;
+	notice_file_size: number;
+	notice_file_link: string;
+}
+
+/**
+ * 상세 페이지에 표시될 공지사항 타입 (고정 여부 선택)
+ */
+export interface NoticeDetail extends BaseNotice {
+	notice_fix_yn?: boolean;
+	user_cd: string;
+	user_nm: string;
+	notice_content: string;
+	notice_file?: NoticeFile;
+}
