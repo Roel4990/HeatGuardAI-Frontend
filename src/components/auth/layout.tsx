1cc360a0523c7ef566c24ff22cd2cf8606cbaaa9
roel <ash49901@gmail.com>
2026-01-29 17:21:20 +0900

    [HOTFIX] next.js 버전 업데이트 및 이에 따른 코드 수정

--- src/components/auth/layout.tsx
+++ src/components/auth/layout.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import RouterLink from 'next/link';
+import Link from 'next/link';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
@@ -23,9 +23,9 @@ export function Layout({ children }: LayoutProps): React.JSX.Element {
     >
       <Box sx={{ display: 'flex', flex: '1 1 auto', flexDirection: 'column' }}>
         <Box sx={{ p: 3 }}>
-          <Box component={RouterLink} href={paths.home} sx={{ display: 'inline-block', fontSize: 0 }}>
+          <Link href={paths.home} style={{ display: 'inline-block', fontSize: 0 }}>
             <DynamicLogo colorDark="light" colorLight="dark" height={32} width={122} />
-          </Box>
+          </Link>
         </Box>
         <Box sx={{ alignItems: 'center', display: 'flex', flex: '1 1 auto', justifyContent: 'center', p: 3 }}>
           <Box sx={{ maxWidth: '450px', width: '100%' }}>{children}</Box>
