e54944164f0db9c0f0148bad4451e8ac913bbcad
roel <ash49901@gmail.com>
2026-02-04 17:52:05 +0900

    [ADD] 미리보기 이미지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,30 @@ import '@/styles/global.css';
 import { UserProvider } from '@/contexts/user-context';
 import { LocalizationProvider } from '@/components/core/localization-provider';
 import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
+import { getSiteURL } from "@/lib/get-site-url";
+
+const siteUrl = getSiteURL()
 
 export const metadata: Metadata = {
+	metadataBase: new URL(siteUrl),
   title: 'HeatGuard AI',
   description: 'AI 기반 폭염 취약지 쿨링포그 최적 배치 시스템',
   icons: {
     icon: '/assets/heatguardLogo.svg',
   },
+	openGraph: {
+		title: 'HeatGuard AI',
+		description: 'AI 기반 폭염 취약지 쿨링포그 최적 배치 시스템',
+		images: [
+			{
+				url: '/nomoreIcon.png',
+				width: 1200,
+				height: 630,
+				alt: 'HeatGuard AI',
+			}
+		],
+		type: 'website'
+	}
 };
 
 export const viewport = { width: 'device-width', initialScale: 1 } satisfies Viewport;
